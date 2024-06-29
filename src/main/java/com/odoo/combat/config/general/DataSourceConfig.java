b31705125be521531e4e2ea1fb215fc849949215
Smit Joshi <smitjoshi814@gmail.com>
2024-06-29 16:39:39 +0530

    datasource logging

--- src/main/java/com/odoo/combat/config/general/DataSourceConfig.java
+++ src/main/java/com/odoo/combat/config/general/DataSourceConfig.java
@@ -2,6 +2,8 @@ package com.odoo.combat.config.general;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
+
+	private final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
+
 	@Value("${spring.datasource.url:}")
 	private final String url = env("SPRING_DATASOURCE_URL");
 	@Value("${spring.datasource.username:}")
@@ -16,13 +21,15 @@ public class DataSourceConfig {
 	@Value("${spring.datasource.password:}")
 	private final String password = env("SPRING_DATASOURCE_PASSWORD");
 
-
 	private static String env(String name) {
 		return System.getenv(name);
 	}
 
 	@Bean
 	DataSource dataSource() {
+		logger.info("datasource url: ", url);
+		logger.info("username: ", username);
+		logger.info("pasword: ", password);
 		return DataSourceBuilder.create().url(url).username(username).password(password).build();
 	}
 }
